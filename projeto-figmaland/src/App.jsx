5d4e2330dd13274ac547e00681c3877ede2c4064
Pablo Henrique <pablohenriqueadm94@gmail.com>
2024-11-02 11:14:15 -0300

    add colors or project

--- projeto-figmaland/src/App.jsx
+++ projeto-figmaland/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
   <>
-    <h1 className="font-graphik">testando</h1>
+    <h1 className="font-graphik-bold bg-darkBackground text-primary">testando</h1>
     <h2 className="font-mono">testando</h2>
   </>
   );
--- projeto-figmaland/tailwind.config.js
+++ projeto-figmaland/tailwind.config.js
@@ -4,8 +4,14 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        graphik: ["Graphik", "sans-serif"],
-        'graphik-bold': ["Graphik-bold", "sans-serif"]
+        'graphik': ["Graphik", "sans-serif"],
+        'graphik-bold': ["Graphik-bold", "sans-serif"],
+      },
+      colors: {
+        primary: '#2091F9',
+        text: '#252B42',
+        secondaryText: '#374754',
+        darkBackground: '#252B42'
       }
     },
   },
